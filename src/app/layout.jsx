e406ef823ccbd5275d0e455b020c5178901c40c5
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-06 21:29:20 +0530

    Add new files and update existing files

---
+++ src/app/layout.jsx
@@ -0,0 +1,30 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default: "DedxDebt - Debt Tracker",
+    template: "%s - DedxDebt",
+  },
+  description: "DedxDebt is a simple debt tracker.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { ToggleTheme } from "@/components/ToggleTheme/ToggleTheme";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <ToggleTheme />
+    </>
+  );
+}
+ 
