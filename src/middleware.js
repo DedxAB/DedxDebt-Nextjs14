709e654a61610e6995e4d82414bd15c9ab8a6f98
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-09 15:26:14 +0530

    Refactor middleware.js to use isProtectedRoute for authentication

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isProtectedRoute = createRouteMatcher([
-//   "/dashboard(.*)",
-//   "/profile(.*)",
-//   "/onboarding",
-//   "/create-ticket",
-// ]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/profile(.*)",
+  "/onboarding",
+  "/create-ticket",
+]);
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+// const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return;
-  auth().protect();
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
