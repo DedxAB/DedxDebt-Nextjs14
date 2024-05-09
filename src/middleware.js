18675c8bd2d5220de52d886922e0020d20fb9e9e
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-09 15:31:07 +0530

    Refactor middleware.js to protect only public routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/profile(.*)",
-  "/onboarding",
-  "/create-ticket",
-]);
+// const isProtectedRoute = createRouteMatcher([
+//   "/dashboard(.*)",
+//   "/profile(.*)",
+//   "/onboarding",
+//   "/create-ticket",
+// ]); // Add more routes as needed
 
-// const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isPublicRoute(req)) return;
+  auth().protect();
 });
 
 export const config = {
