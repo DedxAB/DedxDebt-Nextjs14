8d7daea536d5e7879ec159c6e1bc29d4c4784446
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-13 18:22:08 +0530

    Refactor paymentMode model and userServices***

--- src/models/paymentMode.model.js
+++ src/models/paymentMode.model.js
@@ -8,55 +8,31 @@ const paymentModeSchema = new mongoose.Schema(
       required: [true, "Please provide a user ID"],
     },
     paymentMethod: {
-      upiId: [
-        {
+      upiId: {
+        type: String,
+        required: false,
+        trim: true,
+        lowercase: [true, "UPI ID must be in lowercase"],
+      },
+      upiNumber: {
+        type: String,
+        required: false,
+        trim: true,
+      },
+      bankAccount: {
+        accountNumber: {
           type: String,
           required: false,
-          trim: true,
-          lowercase: [true, "UPI ID must be in lowercase"],
         },
-      ],
-      upiNumber: [
-        {
+        ifsc: {
           type: String,
           required: false,
-          trim: true,
         },
-      ],
-      bankAccount: [
-        {
-          accountNumber: {
-            type: String,
-            required: false,
-          },
-          ifsc: {
-            type: String,
-            required: false,
-          },
-        },
-      ],
+      },
     },
   },
   { timestamps: true }
 );
-
-// Custom validator to ensure at least one payment method is provided
-function atLeastOnePaymentMethod(value) {
-  return (
-    (value.upiId && value.upiId.length > 0) ||
-    (value.upiNumber && value.upiNumber.length > 0) ||
-    (value.bankAccount && value.bankAccount.length > 0)
-  );
-}
-
-// Apply the custom validator to the paymentMethod field
-paymentModeSchema.pre("validate", function (next) {
-  if (!atLeastOnePaymentMethod(this.paymentMethod)) {
-    next(new Error("Please provide at least one payment method"));
-  } else {
-    next();
-  }
-});
 
 const PaymentMode =
   mongoose.models.PaymentMode ||
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -6,11 +6,26 @@ export const fetchUserByClerkId = async (clerkId) => {
       cache: "no-store",
     });
     if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error.message);
+      const { error } = await response.json();
+      throw new Error(error);
     }
     return response.json();
   } catch (error) {
-    console.log(error.message);
+    console.log(`${error.message} from fetchUserByClerkId \n`);
+  }
+};
+
+export const fetchUserPaymentMode = async (lenderId) => {
+  try {
+    const response = await fetch(`${baseUrl}/api/payment-mode/${lenderId}`, {
+      cache: "no-store",
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(`${error} from fetchUserPaymentMode\n`);
   }
 };
