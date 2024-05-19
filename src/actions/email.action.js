703fb20c61eb1af440c54b9e2c75e750a996ba10
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-19 14:56:12 +0530

    Add email update functionality

--- src/actions/email.action.js
+++ src/actions/email.action.js
@@ -3,15 +3,17 @@
 import nodemailer from "nodemailer";
 import { render } from "@react-email/render";
 import Email from "@/email/email";
+import UpdateEmail from "@/email/updateEmail";
 
 export const sendEmail = async ({
+  lenderName,
   borrowerName,
   borrowerEmail,
-  text,
   loanAmount,
   loanDate,
-  lender,
+  loanReason,
 }) => {
+  const customEmailSubject = "New Debt Notification 🚨";
 
   try {
     const transporter = nodemailer.createTransport({
@@ -26,10 +28,11 @@ export const sendEmail = async ({
     });
     const emailHtml = render(
       <Email
+        lenderName={lenderName}
         borrowerName={borrowerName}
         loanAmount={loanAmount}
         loanDate={loanDate}
-        lenderName={lender}
+        loanReason={loanReason}
       />,
       {
         pretty: true,
@@ -42,11 +45,61 @@ export const sendEmail = async ({
         address: process.env.USER_EMAIL,
       },
       to: borrowerEmail,
-      subject: text,
+      subject: customEmailSubject,
       html: emailHtml,
     };
     await transporter.sendMail(options);
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const sendUpdatedTicketEmail = async ({
+  lenderName,
+  borrowerName,
+  borrowerEmail,
+  loanAmount,
+  loanReason,
+  loanDate,
+  customEmailMessage,
+}) => {
+  const customEmailSubject = "Updated Debt Notification 🚨";
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    const emailHtml = render(
+      <UpdateEmail
+        lenderName={lenderName}
+        borrowerName={borrowerName}
+        loanAmount={loanAmount}
+        loanReason={loanReason}
+        loanDate={loanDate}
+        customEmailMessage={customEmailMessage}
+      />,
+      {
+        pretty: true,
+      }
+    );
+
+    const options = {
+      from: {
+        name: "DedxDebt - Debt Manager",
+        address: process.env.USER_EMAIL,
+      },
+      to: borrowerEmail,
+      subject: customEmailSubject,
+      html: emailHtml,
+    };
+    await transporter.sendMail(options);
+  } catch (error) {
+    console.log(error);
   }
 };
